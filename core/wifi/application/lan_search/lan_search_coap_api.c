a08e46b8726451812825c1a46201f20d04c21fa9
zhangkun <zhangkun666@huawei.com>
2024-08-02 10:29:53 +0800

    feat:增加本地发现服务
    Signed-off-by: zhangkun <zhangkun666@huawei.com>

---
+++ core/wifi/application/lan_search/lan_search_coap_api.c
@@ -0,0 +1,290 @@
+/*
+ * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "lan_search_coap_api.h"
+#include "lan_search_ctx.h"
+#include "utils_json.h"
+#include "coap_codec_utils.h"
+#include "iotc_errcode.h"
+#include "securec.h"
+#include "utils_assert.h"
+#include "iotc_svc_dev.h"
+#include "dfx_anonymize.h"
+#include "svc_info_mdl.h"
+#include "lan_search_peer_mngr.h"
+#include "svc_info.h"
+#include "dev_info.h"
+
+static void LanSearchSendJsonResp(CoapEndpoint *endpoint, const CoapPacket *req, const SocketAddr *addr,
+    AdapterJson *respJson, LanSearchSessMsg *sessMsg)
+{
+    CoapServerRespParam respParam = {
+        .req = req,
+        .type = COAP_MSG_TYPE_NCON,
+        .code = COAP_RESPONSE_CODE_CONTENT,
+        .opNum = 0,
+        .options = NULL,
+        .payload = NULL,
+        .payloadBuilder = CoapUtilsBuildJsonPayloadFunc,
+        .payloadUserData = respJson,
+        .preSize = 0,
+    };
+
+    int32_t ret = CoapServerSendResp(endpoint, &respParam, addr, &sessMsg->packet);
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("send json resp msg error %d", ret);
+    }
+    return;
+}
+
+static void LanSearchSendDataResp(CoapEndpoint *endpoint, const CoapPacket *req, const SocketAddr *addr,
+    const CoapData *respData, LanSearchSessMsg *sessMsg)
+{
+    CoapServerRespParam respParam = {
+        .req = req,
+        .type = COAP_MSG_TYPE_NCON,
+        .code = COAP_RESPONSE_CODE_CONTENT,
+        .opNum = 0,
+        .options = NULL,
+        .payload = respData,
+        .payloadBuilder = NULL,
+        .payloadUserData = NULL,
+        .preSize = 0,
+    };
+
+    int32_t ret = CoapServerSendResp(endpoint, &respParam, addr, &sessMsg->packet);
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("send data resp msg error %d", ret);
+    }
+    return;
+}
+
+static bool LanSearchAddDevid(AdapterJson *json)
+{
+    DevAuthInfo authInfo = {0};
+    bool isAuthExits = false;
+    int32_t ret = DevSvcProxyGetAuthInfo(&isAuthExits, &authInfo);
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("get auth info error %d", ret);
+        return false;
+    }
+    if (!isAuthExits) {
+        ret = AdapterJsonAddStr2Obj(json, STR_NETINFO_DEVICE_ID, "");
+    } else {
+        ret = AdapterJsonAddStr2Obj(json, STR_NETINFO_DEVICE_ID, authInfo.devId);
+        (void)memset_s(&authInfo, sizeof(DevAuthInfo), 0, sizeof(DevAuthInfo));
+    }
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("add devid error %d", ret);
+        return false;
+    }
+    return true;
+}
+
+static bool LanSearchAddDevInfo(AdapterJson *json)
+{
+    AdapterJson *devInfoJson = AdapterCreateJson();
+    if (devInfoJson == NULL) {
+        IOTC_LOGW("create json error");
+        return false;
+    }
+
+    int32_t ret = AdapterJsonAddItem2Obj(json, STR_JSON_DEV_INFO, devInfoJson);
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("add dev info json error %d", ret);
+        AdapterJsonDelete(devInfoJson);
+        return false;
+    }
+
+    const IotcDeviceInfo *devInfo = ModelGetDevInfo();
+    if (devInfo == NULL || devInfo->sn == NULL || devInfo->devTypeId == NULL || devInfo->manuId == NULL ||
+        devInfo->prodId == NULL || devInfo->protType == IOTC_PROT_TYPE_INVALID) {
+        IOTC_LOGW("dev info invalid %d", ret);
+        return false;
+    }
+
+    DFX_ANONYMIZE_SN_STR(anonySn, devInfo->sn);
+    UtilsJsonStrItem strTable[] = {
+        { STR_JSON_SN, anonySn },
+        { STR_JSON_DEV_TYPE, devInfo->devTypeId },
+        { STR_JSON_MANU, devInfo->manuId },
+        { STR_JSON_PROD_ID, devInfo->prodId },
+    };
+    ret = UtilsJsonAddStrTable(devInfoJson, strTable, ARRAY_SIZE(strTable));
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("add str to json error %d", ret);
+        return false;
+    }
+
+    ret = AdapterJsonAddNum2Obj(devInfoJson, STR_JSON_PROT_TYPE, devInfo->protType);
+    if (ret != IOTC_OK) {
+        IOTC_LOGE("add prot err %d", ret);
+        return false;
+    }
+    return true;
+}
+
+static bool LanSearchAddSvcInfo(AdapterJson *json)
+{
+    uint32_t svcNum = 0;
+    const IotcServiceInfo *svcInfo = ModelGetSvcInfo(&svcNum);
+    if (svcInfo == NULL || svcNum == 0) {
+        IOTC_LOGW("get svc info error");
+        return false;
+    }
+
+    AdapterJson *svcInfoJson = MdlBuildSvcJsonArray(svcInfo, svcNum);
+    if (svcInfoJson == NULL) {
+        IOTC_LOGW("create svc json error");
+        return false;
+    }
+
+    int32_t ret = AdapterJsonAddItem2Obj(json, STR_JSON_SERVICES, svcInfoJson);
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("add svc info json error %d", ret);
+        AdapterJsonDelete(svcInfoJson);
+        return false;
+    }
+
+    return true;
+}
+
+static AdapterJson *CreateLanSearchRespJson(void)
+{
+    AdapterJson *json = UtilsJsonCreateErrcode(0);
+    if (json == NULL) {
+        IOTC_LOGW("create json error");
+        return NULL;
+    }
+
+    do {
+        if (!LanSearchAddDevid(json)) {
+            break;
+        }
+
+        if (!LanSearchAddDevInfo(json)) {
+            break;
+        }
+
+        if (!LanSearchAddSvcInfo(json)) {
+            break;
+        }
+        return json;
+    } while (0);
+    AdapterJsonDelete(json);
+    return NULL;
+}
+
+void LanSearchCoapSearchHandler(CoapEndpoint *endpoint, const CoapPacket *req, const SocketAddr *addr, void *userData)
+{
+    CHECK_V_RETURN_LOGW(endpoint != NULL && req != NULL && addr != NULL && userData != NULL, "invalid param");
+
+    AdapterJson *respJson = CreateLanSearchRespJson();
+    if (respJson == NULL) {
+        return;
+    }
+
+    LanSearchSessMsg respMsg;
+    (void)memset_s(&respMsg, sizeof(LanSearchSessMsg), 0, sizeof(LanSearchSessMsg));
+    UTILS_BIT_SET(respMsg.bitMap, LAN_SEARCH_SESS_MSG_BIT_PLAIN);
+    LanSearchSendJsonResp(endpoint, req, addr, respJson, &respMsg);
+    AdapterJsonDelete(respJson);
+}
+
+void LanSearchCoapSpekeHandler(CoapEndpoint *endpoint, const CoapPacket *req, const SocketAddr *addr, void *userData)
+{
+    CHECK_V_RETURN_LOGW(endpoint != NULL && req != NULL && addr != NULL && userData != NULL, "invalid param");
+
+    LanSearchContext *ctx = (LanSearchContext *)userData;
+    LanSearchSessMsg *sessMsg = (LanSearchSessMsg *)req;
+
+    if (req->payload.data == NULL || req->payload.len == 0) {
+        IOTC_LOGW("invalid peer sess msg");
+        return;
+    }
+
+    int32_t ret;
+    if (sessMsg->peer == NULL) {
+        ret = LanSearchCreatePeer(ctx, addr->addr, &sessMsg->peer);
+        if (ret != IOTC_OK) {
+            IOTC_LOGW("create peer error %d", ret);
+            return;
+        }
+    }
+    if (sessMsg->peer == NULL || sessMsg->peer->sessInfo.speke == NULL) {
+        IOTC_LOGW("invalid peer");
+        return;
+    }
+
+    uint8_t *respMsg = NULL;
+    uint32_t respLen = 0;
+    ret = SpekeProcessPacket(sessMsg->peer->sessInfo.speke,
+        (const char *)req->payload.data, &respMsg, &respLen);
+    if (ret != IOTC_OK || respMsg == NULL || respLen == 0) {
+        IOTC_LOGW("speke process packet error %d/%u", ret, respLen);
+        if (respMsg != NULL) {
+            AdapterFree(respMsg);
+        }
+        return;
+    }
+
+    CoapData respPayload = {respMsg, respLen};
+    LanSearchSessMsg sendMsg;
+    (void)memset_s(&sendMsg, sizeof(LanSearchSessMsg), 0, sizeof(LanSearchSessMsg));
+    UTILS_BIT_SET(sendMsg.bitMap, LAN_SEARCH_SESS_MSG_BIT_PLAIN);
+    sendMsg.peer = sessMsg->peer;
+    LanSearchSendDataResp(endpoint, req, addr, &respPayload, &sendMsg);
+    AdapterFree(respMsg);
+}
+
+void LanSearchCoapCloudSetupHandler(CoapEndpoint *endpoint, const CoapPacket *req,
+    const SocketAddr *addr, void *userData)
+{
+    CHECK_V_RETURN_LOGW(endpoint != NULL && req != NULL && addr != NULL && userData != NULL &&
+        req->payload.data != NULL && req->payload.len != 0, "invalid param");
+    LanSearchSessMsg *sessMsg = (LanSearchSessMsg *)req;
+
+    AdapterJson *reqJson = AdapterJsonParseWithLen((const char *)req->payload.data, req->payload.len);
+    if (reqJson == NULL) {
+        IOTC_LOGW("cloud setup json parse error");
+        return;
+    }
+
+    AdapterJson *dataJson = AdapterJsonGetObj(reqJson, STR_JSON_DATA);
+    if (dataJson == NULL) {
+        IOTC_LOGW("cloud setup get data error");
+        AdapterJsonDelete(dataJson);
+        return;
+    }
+
+    /* 当前端云未就绪，配网信息中只包含认证信息 */
+    int32_t ret = DevSvcProxyRecvAuthInfo(dataJson);
+    AdapterJsonDelete(reqJson);
+    reqJson = NULL;
+    if (ret != IOTC_OK) {
+        IOTC_LOGW("bind info process error %d", ret);
+    }
+
+    AdapterJson *respJson = UtilsJsonCreateErrcode(ret);
+    if (respJson == NULL) {
+        IOTC_LOGW("create cloud setup resp error %d", ret);
+        return;
+    }
+
+    LanSearchSessMsg sendMsg;
+    (void)memset_s(&sendMsg, sizeof(LanSearchSessMsg), 0, sizeof(LanSearchSessMsg));
+    sendMsg.peer = sessMsg->peer;
+    LanSearchSendJsonResp(endpoint, req, addr, respJson, &sendMsg);
+    AdapterJsonDelete(respJson);
+}
---
+++ core/wifi/application/lan_search/lan_search_ctx.h
@@ -0,0 +1,100 @@
+
+/*
+ * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef LAN_SEARCH_CONTEXT_H
+#define LAN_SEARCH_CONTEXT_H
+#include <stdint.h>
+#include "comm_def.h"
+#include "coap_endpoint.h"
+#include "coap_endpoint_event_source.h"
+#include "utils_hash_map.h"
+#include "security_sess_key.h"
+#include "service_manager.h"
+#include "utils_buffer.h"
+#include "utils_common.h"
+#include "security_speke.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    LAN_SEARCH_SESS_MSG_BIT_PLAIN = 0,
+} LanSearchSessMsgBitMap;
+
+typedef enum {
+    LAN_SEARCH_PEER_BIT_SPEKE_FINISHED = 0,
+} LanSearchPeerBitMap;
+
+typedef enum {
+    LAN_SEARCH_CTX_COAP_SVR_CREATED = 0,
+    LAN_SEARCH_CTX_COAP_SVR_STARTED,
+} LanSearchCtxBitMap;
+
+typedef struct {
+    uint32_t bitMap;
+    struct {
+        uint32_t createTime;
+        uint32_t expireTime;
+    } timeInfo;
+    struct {
+        SpekeSession *speke;
+    } sessInfo;
+    struct {
+        uint32_t addr;
+    } peerInfo;
+} LanSearchPeer;
+
+typedef struct {
+    CoapPacket packet;
+    uint32_t bitMap;
+    LanSearchPeer *peer;
+} LanSearchSessMsg;
+
+typedef struct {
+    uint32_t bitMap;
+    struct {
+        int32_t instanceId;
+        ServiceFinishCallback onFinish;
+    } svcInfo;
+    struct {
+        uint32_t maxPeerNum;
+        uint32_t peerExpireTime;
+    } config;
+    struct {
+        UtilsBufferCtx *sendBuf;
+        UtilsBufferCtx *recvBuf;
+        TransLink *link;
+        TransSess *sess;
+        CoapEndpoint *endpoint;
+        EventSource *coapSource;
+        const char **whiteList;
+        uint32_t whiteListNum;
+    } coapServer;
+    struct {
+        uint32_t curPeerNum;
+        int32_t expireCheckTimer;
+        HashMap *peerMap;
+    } peerManager;
+} LanSearchContext;
+
+LanSearchContext *GetLanSearchCtx(void);
+
+int32_t LanSearchContextInit(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* LAN_SEARCH_CONTEXT_H */

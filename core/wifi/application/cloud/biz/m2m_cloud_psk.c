599e1131ef29ff476463c40396b214282efc839d
jike <jike@yarward.com>
2026-03-30 17:25:23 +0800

    增加devame 修改编译报错 修改psk计算密码长度错误 修改option与payload中间0xFF分割错误
    Signed-off-by: jike <jike@yarward.com>

--- core/wifi/application/cloud/biz/m2m_cloud_psk.c
+++ core/wifi/application/cloud/biz/m2m_cloud_psk.c
@@ -33,6 +33,7 @@
 #include "iotc_aes.h"
 #include "config_authinfo.h"
 
+#define PSK_PASSWORD_LEN 16
 
 uint8_t sn1[SESS_SN_LEN] = {0};
 uint8_t sn2[SESS_SN_LEN] = {0};
@@ -170,8 +171,7 @@ int32_t StationSessKeyGen(M2mCloudContext *ctx, const uint8_t *sn1, uint32_t sn1
         .md = IOTC_MD_SHA256,
         .password = ((UTILS_IS_BIT_SET(ctx->bitMap, M2M_CLOUD_CTX_BIT_REGISTER)) ? 
             ctx->authInfo.regInfo.psk : ctx->authInfo.loginInfo.psk),
- 	    .passwordLen = sizeof(((UTILS_IS_BIT_SET(ctx->bitMap, M2M_CLOUD_CTX_BIT_REGISTER)) ? 
-            ctx->authInfo.regInfo.psk : ctx->authInfo.loginInfo.psk)),
+ 	    .passwordLen = PSK_PASSWORD_LEN, 
         .salt = ctx->pskInfo.salt,
         .saltLen = SALT_LEN,
         .iterCount = ITER_TIMES
